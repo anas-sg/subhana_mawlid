2ffc26954d7bdf4be2335bba2ab1d9f7c980e13a
Muhammed Anas <anas.m6@gmail.com>
2022-09-14 22:04:04 +0800

    Added favicons

--- 4.php
+++ 4.php
@@ -9,7 +9,17 @@
         <title>Subḥāna Mawlid - 4. Maa Zaala Noor Muhammadin Mutanaqqila</title>
         <link href="css/bootstrap.min.css" type="text/css" rel="stylesheet">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.3/font/bootstrap-icons.css">
-        <link href="css/style.css" type="text/css" rel="stylesheet">    
+        <link href="css/style.css" type="text/css" rel="stylesheet">
+        <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
+		<link rel="apple-touch-icon" href="img/apple-touch-icon.png">
+		<link rel="apple-touch-icon" sizes="57x57" href="img/apple-touch-icon-57x57.png">
+		<link rel="apple-touch-icon" sizes="72x72" href="img/apple-touch-icon-72x72.png">
+		<link rel="apple-touch-icon" sizes="76x76" href="img/apple-touch-icon-76x76.png">
+		<link rel="apple-touch-icon" sizes="114x114" href="img/apple-touch-icon-114x114.png">
+		<link rel="apple-touch-icon" sizes="120x120" href="img/apple-touch-icon-120x120.png">
+		<link rel="apple-touch-icon" sizes="144x144" href="img/apple-touch-icon-144x144.png">
+		<link rel="apple-touch-icon" sizes="152x152" href="img/apple-touch-icon-152x152.png">
+		<link rel="apple-touch-icon" sizes="180x180" href="img/apple-touch-icon-180x180.png">    
   	</head>
   	<body class="d-flex text-center bg-dark">    
 		<div class="cover-container d-flex w-100 mx-auto flex-column text-white">
